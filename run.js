737b6a4748b4e1c4aea2ff435f439e87c8959e09
xxxsjan <626653354@qq.com>
2023-07-22 01:30:03 +0800

    优化脚本

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
-const { exec, execFileSync, execFile, spawn } = require('child_process');
-const { futimesSync } = require('fs');
-const shell = require('shelljs');
+const { exec, execFileSync, execFile, spawn } = require("child_process");
+const { futimesSync } = require("fs");
+const shell = require("shelljs");
 
 // 解析命令的库
 // const program = require('commander');
@@ -11,54 +11,43 @@ const shell = require('shelljs');
 // 不使用库，简单解析
 // process.argv 第一个是node执行器的路径，第二个是当前执行js的文件路径，第三个起才是option
 
-
 switch (process.argv[2]) {
-  case '1':
+  case "1":
     getFollowList();
     break;
-  case '2':
+  case "2":
     getNot();
     break;
-  case '3':
+  case "3":
     analysisData();
     break;
   default:
     console.log(
-      '传参不对 \n',
-      'node ./run.js 1 -----get follow list\n',
-      'node ./run.js 2 -----get note follow me \n',
-      'node ./run.js 3 -----analysis data \n'
+      "传参不对 \n",
+      "node ./run.js 1 -----get follow list\n",
+      "node ./run.js 2 -----get note follow me \n",
+      "node ./run.js 3 -----analysis data \n"
     );
     break;
 }
 
 function getFollowList() {
-  shell.exec('node ./src/get-follow-list.js', function (code, stdout, stderr) {
-    console.log('Exit code:', code);
-    console.log('Program output:', stdout);
-    console.log('Program stderr:', stderr);
-    if (code === 0) {
-      console.log('code: ', code);
-    }
-  });
+  handleShell("node ./src/get-follow-list.js");
 }
 function getNot() {
-  shell.exec('node ./src/get-not-follow-me.js', function (code, stdout, stderr) {
-    console.log('Exit code:', code);
-    console.log('Program output:', stdout);
-    console.log('Program stderr:', stderr);
-    if (code === 0) {
-      console.log('code: ', code);
-    }
-  });
+  handleShell("node ./src/get-not-follow-me.js");
 }
 function analysisData() {
-  shell.exec('node ./src/analysis-follow.js', function (code, stdout, stderr) {
-    console.log('Exit code:', code);
-    console.log('Program output:', stdout);
-    console.log('Program stderr:', stderr);
+  handleShell("node ./src/analysis-follow.js");
+}
+
+function handleShell(command) {
+  shell.exec(command, function (code, stdout, stderr) {
+    console.log("Exit code:", code);
+    console.log("Program output:", stdout);
+    console.log("Program stderr:", stderr);
     if (code === 0) {
-      console.log('code: ', code);
+      console.log("code: ", code);
     }
   });
 }
