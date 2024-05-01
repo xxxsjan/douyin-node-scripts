063f46705d8d41729e006c06b74bc21cb8a69ecc
xxxsjan <626653354@qq.com>
2024-05-01 15:49:21 +0800

    优化代码结构，更新配置文件和工具函数。

--- run.js
+++ run.js
@@ -1,45 +1,7 @@
 const { exec, execFileSync, execFile, spawn } = require("child_process");
-const { futimesSync } = require("fs");
 const shell = require("shelljs");
 
-// 解析命令的库
-// const program = require('commander');
-// program.option('-p,--port [type] ', 'Add port ').parse(process.argv);
-// if (program.port) port = program.port;
-// console.log('imovie start on port ' + port);
-
-// 不使用库，简单解析
-// process.argv 第一个是node执行器的路径，第二个是当前执行js的文件路径，第三个起才是option
-
-switch (process.argv[2]) {
-  case "1":
-    getFollowList();
-    break;
-  case "2":
-    getNot();
-    break;
-  case "3":
-    analysisData();
-    break;
-  default:
-    console.log(
-      "传参不对 \n",
-      "node ./run.js 1 -----get follow list\n",
-      "node ./run.js 2 -----get note follow me \n",
-      "node ./run.js 3 -----analysis data \n"
-    );
-    break;
-}
-
-function getFollowList() {
-  handleShell("node ./src/get-follow-list.js");
-}
-function getNot() {
-  handleShell("node ./src/get-not-follow-me.js");
-}
-function analysisData() {
-  handleShell("node ./src/analysis-follow.js");
-}
+console.log(process.argv);
 
 function handleShell(command) {
   shell.exec(command, function (code, stdout, stderr) {
