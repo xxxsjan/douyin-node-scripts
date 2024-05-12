85672af5ea80a04367a148365727b70c1e4ed572
xxxsjan <626653354@qq.com>
2024-05-13 01:26:05 +0800

    删除等待并点击音频按钮的操作

--- test/test-url.js
+++ test/test-url.js
@@ -6,13 +6,4 @@ const ms = require("ms");
   const { page } = await createPuppeteer();
 
   await page.goto("https://live.douyin.com/243107977808");
-
-  const voiceBtn = await page
-    .waitForSelector(".mLnbv9qu.pmBw8k1t  .ZblGNktR2", { timeout: 5000 })
-    .catch((err) => {
-      console.log("err: ", err);
-    });
-
-  await delay(ms("1s"));
-  voiceBtn.click();
 })();
