0d5c823030896057c3a7fa24ac7a4b4ce50cd739
xxxsjan <626653354@qq.com>
2024-05-13 12:03:10 +0800

    修复 find-notFollow 删除脚本中的错误，优化逻辑并增加日志功能。

--- packages/find-notFollow/delete.js
+++ packages/find-notFollow/delete.js
@@ -1,13 +1,12 @@
-const puppeteer = require("puppeteer");
 const { delay } = require("bluebird");
 const ms = require("ms");
-const path = require("path");
 var log = require("single-line-log").stdout;
-const { createPuppeteer } = require("../../utils/createPuppeteer");
-const folderPath = path.resolve(__dirname, "cache");
-const lacalPath = path.join(folderPath, "notFollow-result.json");
 
-const notFollowData = require(lacalPath) || [];
+const { initDb } = require("./db");
+const { createPuppeteer, pclog } = require("utils");
+
+const db = initDb();
+const notFollowData = db.get("notFollowList").value();
 
 run();
 
@@ -16,7 +15,7 @@ async function run() {
     if (notFollowData.length === 0) {
       return;
     }
-    const { browser, page } = await createPuppeteer();
+    const { page } = await createPuppeteer();
 
     await walk(0, notFollowData.length - 1);
 
@@ -27,10 +26,8 @@ async function run() {
 
       const cur = notFollowData[curIdx];
 
-      const pattern = /^https:\/\/www\.douyin\.com\/user\/.+$/;
-
-      if (!pattern.test(cur.homeUrl)) {
-        console.log("链接不符合格式❌", cur);
+      if (!cur.homeUrl) {
+        console.log("链接不符合格式❌", cur.nickname);
         await walk(curIdx + 1, max);
         return;
       }
@@ -38,32 +35,49 @@ async function run() {
       await page.goto(cur.homeUrl);
       await page.waitForSelector(".ty_H89Vr");
 
-      const msg = await page.evaluate(({}) => {
+      const msg = await page.evaluate(() => {
         // 粉丝数
         const fsEl = document.querySelector(
-          '.WetwxGAE[data-e2e="user-info-like"] .sCnO6dhe'
+          '.WetwxGAE[data-e2e="user-info-fans"] .sCnO6dhe'
         );
         // 昵称
         const nameEl = document.querySelector(
           ".ds1zWG9C .j5WZzJdp span span span span"
         );
-        const fensiText = fsEl.textContent;
+        // 关注
+        const followEl = document.querySelector(
+          '.WetwxGAE[data-e2e="user-info-follow"] .sCnO6dhe'
+        );
+
+        const username = nameEl.textContent;
+        const fensiText = fsEl ? fsEl.textContent : "";
+        const followText = followEl ? followEl.textContent : "";
+
         if (
           fensiText &&
           (fensiText.includes("万") || fensiText.includes("亿"))
         ) {
-          return nameEl.textContent + "," + fensiText + ",大网红，不删除❌";
+          return `✅ ${username} ${fensiText}大网红，不删除`;
         } else {
-          // 已关注按钮
-          const el = document.querySelector(
-            ".ZBejQ5yK.WXPQLGYI.UNy4jPPO.ajC8cNxV.I4tJiW0Q.EE_LhMCF"
-          );
-          el && el.click();
-          return nameEl.textContent + "已移除✅" + "," + fensiText;
+          const followNum = parseFloat(followText);
+          const fensiNum = parseFloat(fensiText);
+          if (followNum < fensiNum / 10) {
+            // 已关注按钮
+            // const el = document.querySelector(
+            //   ".ZBejQ5yK.WXPQLGYI.UNy4jPPO.ajC8cNxV.I4tJiW0Q.EE_LhMCF"
+            // );
+            // el && el.click();
+            return `❌ ${username} [${followText} ${fensiText}] 已移除 `;
+          } else {
+            return `✅ ${username} [${followText} ${fensiText}] 符合互关条件，不删除`;
+          }
         }
-      }, {});
-      console.log(msg, Date.now());
+      });
+
+      console.log(getCurrentTime(), msg);
+
       await delay(ms("1s"));
+
       await walk(curIdx + 1, max);
     }
 
@@ -73,4 +87,8 @@ async function run() {
   } catch (error) {
     log("浏览器问题: ");
   }
+}
+function getCurrentTime() {
+  const now = new Date();
+  return now.toLocaleTimeString();
 }
